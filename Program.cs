4bd9adcde64217d5979c6ba7a83afcabcd97d36e
Giovanni A. L. Oliveira <giovanni.al.oliveira@gmail.com>
2024-04-12 18:25:48 -0300

    feat: add code to simulate a reservation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using trilha_net_explorando_desafio.Models;
+
+var giovanni = new Person("Giovanni", "Oliveira");
+var dayane = new Person("Dayane", "Ferreira");
+
+var guests = new List<Person>() { giovanni, dayane };
+
+var suite = new Suite("Premium", 2, 140.00M);
+
+var reservation = new Reservation(suite, 10);
+
+try
+{
+    reservation.RegisterGuests(guests);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+Console.WriteLine($"Suite: {suite.Type}");
+Console.WriteLine($"Total de hóspedes: {reservation.GetNumberOfGuests()}");
+Console.WriteLine($"Valor total: {reservation.CalculateDailyRate():C}");
