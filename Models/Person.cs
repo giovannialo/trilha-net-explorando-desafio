462dd624e9ad6d9f91b09ad2713aa39f59bf6d4d
Giovanni A. L. Oliveira <giovanni.al.oliveira@gmail.com>
2024-04-12 18:12:04 -0300

    feat: add Person model class

---
+++ Models/Person.cs
@@ -0,0 +1,13 @@
+namespace trilha_net_explorando_desafio.Models;
+
+public class Person
+{
+    public string Name { get; set; }
+    public string Surname { get; set; }
+
+    public Person(string name, string surname)
+    {
+        Name = name;
+        Surname = surname;
+    }
+}
